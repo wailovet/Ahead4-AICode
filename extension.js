9dc185d1c799f05654876d7c99a5434876ecd527
u <u@a.com>
2022-10-24 18:12:47 +0800

    fix

--- extension.js
+++ extension.js
@@ -1,23 +1,34 @@
 const vscode = require('vscode');
 const axios = require("axios")
 
-var url = "http://119.28.6.41:8182/AI-completion"
-// var url = "http://127.0.0.1:46188/AI-completion"
+// var url = "http://119.28.6.41:8182/AI-completion"
+var url = "http://127.0.0.1:46188/AI-completion"
 
 
-var id = ""
 exports.activate = function () {
+
+	var StatusBar
+	var Locker = false
+	StatusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+	StatusBar.text = "AHead4:空闲"
+	StatusBar.show()
+
 	vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, {
 		provideInlineCompletionItems: async (document, position, context, token) => {
+			if (Locker) {
+				return
+			}
+			Locker = true
 			let cid = String(Math.random())
 			id = cid
-			await new Promise(resolve => setTimeout(resolve, 1000));
+			await new Promise(resolve => setTimeout(resolve, 2000));
 			if (id != cid) {
 				console.log("防抖动")
 				return
 			}
-
-			console.log("无抖动")
+			StatusBar.text = "AHead4:忙碌"
+			console.log("无抖动") 
+			console.log('provideInlineCompletionItems offsetAt:', document.offsetAt(position)) 
 			const fileName = document.fileName;
 			const src = document.getText();
 			// const word = document.getText(document.getWordRangeAtPosition(position));
@@ -37,13 +48,14 @@ exports.activate = function () {
 					language: "go",
 					line_num: String(position.line),
 					character_num: String(position.character),
+					offset: String(document.offsetAt(position)),
 				},  //这里json对象会转换成json格式字符串发送
 				header: {
 					'Content-Type': 'application/json'  //如果写成contentType会报错,如果不写这条也报错
 					//Content type 'application/x-www-form-urlencoded;charset=UTF-8'...
 				}
 			}).then(res => {
-				// console.log(res);
+				console.log(res);
 				var ret = res["data"]
 				if (ret['code'] == 0) {
 					return ret["data"];
@@ -58,6 +70,9 @@ exports.activate = function () {
 			// console.log('complete3: ', item);
 
 			var output = item
+
+			StatusBar.text = "AHead4:空闲"
+			Locker = false
 			return [
 				new vscode.InlineCompletionItem(output, new vscode.Range(position, position.translate(0, output.length)),),
 			];
