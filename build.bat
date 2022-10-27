cf9dca7dd9e3080358c3b56e347352444c03ec21
u <u@a.com>
2022-10-27 18:06:36 +0800

    fix

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 
 vsce package
+pause
--- extension.js
+++ extension.js
@@ -1,89 +1,90 @@
 const vscode = require('vscode');
 const axios = require("axios")
 
-var url = "http://119.28.6.41:8182/AI-completion"
-// var url = "http://127.0.0.1:46188/AI-completion"
 
 
 exports.activate = function () {
+	var url = vscode.workspace.getConfiguration('AHead4.url').get()
+	var token = vscode.workspace.getConfiguration('AHead4.token').get() 
+	if (!url || url == "") {
+		url = "http://119.28.6.41:8182/AI-completion"
+		if (!token || token == "") {
+			token = "de676ffb5670fbac425e748d7603c472"
+		}
+	}  
+
 
 	var StatusBar
 	var Locker = false
 	StatusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
-	StatusBar.text = "AHead4:空闲"
+	StatusBar.text = "AHead4:idle"
 	StatusBar.show()
 
 	vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, {
 		provideInlineCompletionItems: async (document, position, context, token) => {
-		
-			let cid = String(Math.random())
-			id = cid
-			await new Promise(resolve => setTimeout(resolve, 2000));
-			if (id != cid) {
-				console.log("防抖动")
-				return
-			}
-			if (token.isCancellationRequested){
-				console.log("忽略取消")
-				return
-			}
 
-			if (Locker) {
-				return
-			}
-			Locker = true
-			StatusBar.text = "AHead4:忙碌"
-			console.log("无抖动") 
-			console.log('provideInlineCompletionItems token:', token) 
-			console.log('provideInlineCompletionItems context:', context) 
-			console.log('provideInlineCompletionItems offsetAt:', document.offsetAt(position)) 
-			const fileName = document.fileName;
-			const src = document.getText();
-			// const word = document.getText(document.getWordRangeAtPosition(position));
-			const line = document.lineAt(position);
-
-			// console.log('provideInlineCompletionItems fileName:', fileName);
-			// console.log('provideInlineCompletionItems word:', word);
-			// console.log('provideInlineCompletionItems position:', position);
-			// console.log('provideInlineCompletionItems position.line:', position.line);
-			// console.log('provideInlineCompletionItems line:', line.text);
-
-			var item = await axios({
-				url: url,
-				method: 'post',
-				data: {
-					src: src,
-					language: "go",
-					line_num: String(position.line),
-					character_num: String(position.character),
-					offset: String(document.offsetAt(position)),
-				},  //这里json对象会转换成json格式字符串发送
-				header: {
-					'Content-Type': 'application/json'  //如果写成contentType会报错,如果不写这条也报错
-					//Content type 'application/x-www-form-urlencoded;charset=UTF-8'...
+			try {
+				let cid = String(Math.random())
+				id = cid
+				await new Promise(resolve => setTimeout(resolve, 2000));
+				if (id != cid) {
+					console.log("防抖动")
+					return
 				}
-			}).then(res => {
-				console.log(res);
-				var ret = res["data"]
-				if (ret['code'] == 0) {
-					return ret["data"];
+				if (token.isCancellationRequested) {
+					console.log("忽略取消")
+					return
 				}
-				return undefined;
-			}).catch(error => {
-				console.log(error);
-				return undefined;
-			})
-
-			// console.log('complete3: ', Trim(item));
-			// console.log('complete3: ', item);
-
-			var output = item
-
-			StatusBar.text = "AHead4:空闲"
-			Locker = false
-			return [
-				new vscode.InlineCompletionItem(output, new vscode.Range(position, position.translate(0, output.length)),),
-			];
+	
+				if (Locker) {
+					return
+				}
+				Locker = true
+				StatusBar.text = "AHead4:busy"
+	
+				const fileName = document.fileName;
+				const src = document.getText();
+	
+				var item = await axios({
+					url: url,
+					method: 'post',
+					data: {
+						src: src,
+						token: token,
+						filename: fileName,
+						language_id: document.languageId,
+						offset: String(document.offsetAt(position)),
+					},
+					header: {
+						'Content-Type': 'application/json'
+					}
+				}).then(res => {
+					console.log(res);
+					var ret = res["data"]
+					if (ret['code'] == 0) {
+						return ret["data"];
+					}
+					return undefined;
+				}).catch(error => {
+					console.log(error);
+					return undefined;
+				})
+	
+				StatusBar.text = "AHead4:idle"
+				Locker = false
+				var output = item
+				if (!output || output == "") { 
+					return []
+				}
+	
+	
+				return [
+					new vscode.InlineCompletionItem(output, new vscode.Range(position, position.translate(0, output.length)),),
+				];
+			} catch (error) {
+				console.log(error)
+				vscode.window.showErrorMessage(error)
+			}
 		},
 	})
 
